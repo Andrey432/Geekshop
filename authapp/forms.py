879438cd81738a33876db56b1551b25924a0b3a0
Andrey <zylkovskiy.andrey@mail.ru>
2021-03-15 14:55:27 +0300

    Валидаторы

--- authapp/forms.py
+++ authapp/forms.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import forms
 from authapp.models import ShopUser
+import re
 
 
 class ShopUserLoginForm(forms.AuthenticationForm):
@@ -23,6 +24,26 @@ class ShopUserRegisterForm(forms.UserCreationForm):
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control'
 
+    def clean_first_name(self):
+        first_name = self.cleaned_data['first_name']
+        print(first_name)
+        if not (re.match(r'[\D]*', first_name) and re.match(r'[\w]*', first_name)):
+            raise forms.ValidationError('Имя должно содержать только буквы')
+        return first_name
+
+    def clean_age(self):
+        age = self.cleaned_data['age']
+        if age < 18:
+            raise forms.ValidationError('Услуги сайта доступны только пользователям, старше 18 лет')
+        return age
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        user = ShopUser.objects.filter(email=email).first()
+        if user is not None and user.email != email:
+            raise forms.ValidationError('Данный email уже используется другим аккаунтом')
+        return email
+
 
 class ShopUserEditForm(forms.UserChangeForm):
     class Meta:
@@ -33,3 +54,23 @@ class ShopUserEditForm(forms.UserChangeForm):
         super().__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control'
+
+    def clean_first_name(self):
+        first_name = self.cleaned_data['first_name']
+        if not (re.fullmatch(r'[\D]*', first_name) and re.fullmatch(r'[\w]*', first_name)):
+            raise forms.ValidationError('Имя должно содержать только буквы')
+        return first_name
+
+    def clean_age(self):
+        age = self.cleaned_data['age']
+        # print(self.cleaned_data['first_name'])
+        if age < 18:
+            raise forms.ValidationError('Услуги сайта доступны только пользователям, старше 18 лет')
+        return age
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        user = ShopUser.objects.filter(email=email).first()
+        if user is not None and user.email != email:
+            raise forms.ValidationError('Данный email уже используется другим аккаунтом')
+        return email

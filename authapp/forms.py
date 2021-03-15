10d14edc01a73d4335a290dc9abb629a919be568
Andrey <zylkovskiy.andrey@mail.ru>
2021-03-15 10:36:27 +0300

    Реализация CRUD

---
+++ authapp/forms.py
@@ -0,0 +1,35 @@
+from django.contrib.auth import forms
+from authapp.models import ShopUser
+
+
+class ShopUserLoginForm(forms.AuthenticationForm):
+    class Meta:
+        model = ShopUser
+        fields = ('username', 'password')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for _, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+
+class ShopUserRegisterForm(forms.UserCreationForm):
+    class Meta:
+        model = ShopUser
+        fields = ('username', 'first_name', 'password1', 'password2', 'email', 'age', 'avatar')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+
+class ShopUserEditForm(forms.UserChangeForm):
+    class Meta:
+        model = ShopUser
+        fields = ('username', 'first_name', 'email', 'age', 'avatar', 'password')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
--- authapp/views.py
+++ authapp/views.py
@@ -1,3 +1,59 @@
+from django.contrib import auth
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
-# Create your views here.
+from authapp.forms import *
+
+
+def login(request):
+    form = ShopUserLoginForm(data=request.POST)
+    if request.method == 'POST' and form.is_valid():
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+        if user and user.is_active:
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse('home'))
+
+    context = {
+        "title": "вход",
+        "login_form": form,
+    }
+    return render(request, 'authapp/login.html', context=context)
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('home'))
+
+
+def register(request):
+    form = ShopUserRegisterForm()
+    if request.method == 'POST':
+        form = ShopUserRegisterForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('auth:login'))
+
+    context = {
+        "title": "регистрация",
+        "register_form": form,
+    }
+    return render(request, 'authapp/register.html', context=context)
+
+
+def edit(request):
+    form = ShopUserEditForm(instance=request.user)
+    if request.method == 'POST':
+        form = ShopUserEditForm(request.POST, request.FILES, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('home'))
+
+    context = {
+        "title": "редактирование",
+        "edit_form": form,
+    }
+    return render(request, 'authapp/edit.html', context=context)
